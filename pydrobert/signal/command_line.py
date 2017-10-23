6c42034704598dde45945611e5528de13db994a0
Sean Robertson <sdrobert@cs.toronto.edu>
2017-10-23 18:05:10 -0400

    Made compute-feats-from-kaldi-tables use kaldi command line utils

--- pydrobert/signal/command_line.py
+++ pydrobert/signal/command_line.py
@@ -15,6 +15,15 @@ from pydrobert.signal.compute import FrameComputer
 from pydrobert.signal.pre import PreProcessor
 from pydrobert.signal.util import alias_factory_subclass_from_arg
 
+try:
+    from pydrobert.kaldi.command_line import kaldi_vlog_level_cmd_decorator
+    from pydrobert.kaldi.command_line import kaldi_logger_decorator
+except ImportError:
+    def kaldi_vlog_level_cmd_decorator(func):
+        return func
+    def kaldi_logger_decorator(func):
+        return func
+
 __author__ = "Sean Robertson"
 __email__ = "sdrobert@cs.toronto.edu"
 __license__ = "Apache 2.0"
@@ -46,146 +55,8 @@ def nonneg_int_type(string):
             '{} is not a nonnegative integer'.format(string))
     return val
 
-def kaldi_rspecifier_type(string):
-    '''Make sure string is a valid rspecifier
-
-    Raises
-    ------
-    ImportError
-        If pydrobert-kaldi is not installed
-    '''
-    from pydrobert.kaldi.io.enums import TableType
-    from pydrobert.kaldi.io.util import parse_kaldi_input_path
-    table_type, _, _, _ = parse_kaldi_input_path(string)
-    if table_type == TableType.NotATable:
-        raise argparse.ArgumentTypeError(
-            '{} is not a valid rspecifier'.format(string))
-    return string
-
-def kaldi_wspecifier_type(string):
-    '''Make sure string is a valid wspecifier
-
-    Raises
-    ------
-    ImportError
-        If pydrobert-kaldi is not installed
-    '''
-    from pydrobert.kaldi.io.enums import TableType
-    from pydrobert.kaldi.io.util import parse_kaldi_output_path
-    table_type, _, _, _ = parse_kaldi_output_path(string)
-    if table_type == TableType.NotATable:
-        raise argparse.ArgumentTypeError(
-            '{} is not a valid wspecifier'.format(string))
-    return string
-
-def kaldi_vlog_level_cmd_decorator(func):
-    '''Decorator to rename, then revert, level names according to Kaldi [1]_
-
-    See pydrobert.kaldi for the conversion chart. After the return of
-    the function, the level names before the call are reverted. This
-    function is insensitive to renaming while the function executes
-
-    .. [1] Povey, D., et al (2011). The Kaldi Speech Recognition
-           Toolkit. ASRU
-    '''
-    def _new_cmd(*args, **kwargs):
-        __doc__ = func.__doc__
-        old_level_names = [logging.getLevelName(0)]
-        for level in range(1, 10):
-            old_level_names.append(logging.getLevelName(level))
-            logging.addLevelName(level, 'VLOG [{:d}]'.format(11 - level))
-        for level in range(10, 51):
-            old_level_names.append(logging.getLevelName(level))
-            if level // 10 == 1:
-                logging.addLevelName(level, 'VLOG [1]')
-            elif level // 10 == 2:
-                logging.addLevelName(level, 'LOG')
-            elif level // 10 == 3:
-                logging.addLevelName(level, 'WARNING')
-            elif level // 10 == 4:
-                logging.addLevelName(level, 'ERROR')
-            elif level // 10 == 5:
-                logging.addLevelName(level, 'ASSERTION_FAILED ')
-        try:
-            ret = func(*args, **kwargs)
-        finally:
-            for level, name in enumerate(old_level_names):
-                logging.addLevelName(level, name)
-        return ret
-    return _new_cmd
-
-def _kaldi_argparse_boilerplate(descr, parent, args):
-    '''Boilerplate surrounding argument creation for kaldi-like scripts
-
-    Arguments
-    ---------
-    descr : str
-        Passed to the parser
-    parent : argparse.ArgumentParser
-        The parent parser from which all arguments are taken. Parent
-        should not have help flag
-    args
-        The arguments to parse
-
-    Returns
-    ------
-    tuple or None
-        If there was an error, returns None. Otherwise, a pair of
-        (namespace, logger)
-    '''
-    prog_name = path.basename(sys.argv[0])
-    class _StdErrHelpAction(argparse.Action):
-        def __init__(self, *args, **kwargs):
-            self.logger = None
-            super(_StdErrHelpAction, self).__init__(*args, **kwargs)
-        def __call__(self, parser, *args, **kwargs):
-            parser.print_help(file=sys.stderr)
-            parser.exit()
-    logging.captureWarnings(True)
-    stderr_handler = logging.StreamHandler()
-    stderr_handler.setFormatter(logging.Formatter(
-        '%(levelname)s (' + prog_name + '[0.0]:%(funcName)s():'
-        '%(filename)s:%(lineno)d) %(message)s'
-    ))
-    i_logger = logging.getLogger('{}.{}.init'.format(__name__, prog_name))
-    i_logger.addHandler(stderr_handler)
-    parser = argparse.ArgumentParser(
-        description=descr,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        add_help=False, parents=[parent],
-        fromfile_prefix_chars='@', # for kaldi-like config files
-    )
-    parser.add_argument(
-        '-h', '--help', action=_StdErrHelpAction, default=argparse.SUPPRESS,
-        nargs=0,
-    )
-    parser.add_argument(
-        '-v', '--verbose', type=int, choices=[x for x in range(-3, 10)],
-        default=0, help='Verbose level (higher->more logging). ',
-    )
-    try:
-        namespace = parser.parse_args(args)
-    except SystemExit:
-        i_logger.error("Failed to parse arguments")
-        return None
-    try:
-        from pydrobert.kaldi.logging import KaldiLogger
-    except ImportError:
-        i_logger.error('Unable to import pydrobert.kaldi.')
-        return None
-    old_logger_class = logging.getLoggerClass()
-    logging.setLoggerClass(KaldiLogger)
-    k_logger = logging.getLogger('{}.{}'.format(__name__, prog_name))
-    logging.setLoggerClass(old_logger_class)
-    k_logger.addHandler(stderr_handler)
-    i_logger.removeHandler(stderr_handler)
-    if namespace.verbose <= 1:
-        k_logger.setLevel(namespace.verbose * -10 + 20)
-    else:
-        k_logger.setLevel(11 - namespace.verbose)
-    return namespace, k_logger
-
 @kaldi_vlog_level_cmd_decorator
+@kaldi_logger_decorator
 def compute_feats_from_kaldi_tables(args=None):
     '''Store features from a kaldi archive in a kaldi archive
 
@@ -195,14 +66,22 @@ def compute_feats_from_kaldi_tables(args=None):
     .. [1] Povey, D., et al (2011). The Kaldi Speech Recognition
            Toolkit. ASRU
     '''
+    from pydrobert.kaldi.command_line import KaldiParser
+    from pydrobert.kaldi.logging import register_logger_for_kaldi
+    logger = logging.getLogger(sys.argv[0])
+    logger.addHandler(logging.StreamHandler())
+    register_logger_for_kaldi(logger)
     # parse arguments
-    parser = argparse.ArgumentParser(add_help=False)
+    parser = KaldiParser(
+        description=compute_feats_from_kaldi_tables.__doc__,
+        add_verbose=True, logger=logger,
+    )
     parser.add_argument(
-        'wav_rspecifier', type=kaldi_rspecifier_type,
+        'wav_rspecifier', type='kaldi_rspecifier',
         help='Input wave table rspecifier'
     )
     parser.add_argument(
-        'feats_wspecifier', type=kaldi_wspecifier_type,
+        'feats_wspecifier', type='kaldi_wspecifier',
         help='Output feature table wspecifier'
     )
     parser.add_argument(
@@ -223,15 +102,10 @@ def compute_feats_from_kaldi_tables(args=None):
         help='JSON list of configurations for pydrobert.signal.pre.PreProcessor'
         ' objects. Audio will be preprocessed in the same order as the list'
     )
-    ret = _kaldi_argparse_boilerplate(
-        compute_feats_from_kaldi_tables.__doc__,
-        parser,
-        args,
-    )
-    if ret is None:
-        return 0
-    else:
-        namespace, logger = ret
+    try:
+        options = parser.parse_args(args)
+    except SystemExit as ex:
+        return ex.code
     # construct the computer
     try:
         computer = alias_factory_subclass_from_arg(
