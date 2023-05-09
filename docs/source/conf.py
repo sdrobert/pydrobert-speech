acbe2f1c6183d3da88c670fc20689d33220c4c60
Sean Robertson <sdrobert@cs.toronto.edu>
2023-05-09 16:15:34 -0400

    Ok, you working now?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ language = "en"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.intersphinx",
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,6 +1,5 @@
-================
-pydrobert-speech
-================
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
 
 .. toctree::
    :maxdepth: 2
